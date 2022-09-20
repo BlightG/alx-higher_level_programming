b619cd1843f87f9fe4bbc922eaaa1e5ed27f540e
BlightG <birhane.gulilat@gmail.com>
2022-09-20 04:31:36 -0700

    added newline charachet to ende of str in task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+    str = str + "\n"
     for i in str:
         if ord(i) in range(97, 123):
             print("{}".format(chr(ord(i) - 32)), end="")
         else:
             print("{}".format(i), end="")
-    print("")
