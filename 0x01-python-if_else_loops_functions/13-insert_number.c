cde88d92a7725311a87b21b52f727937f9ac606b
BlightG <birhane.gulilat@gmail.com>
2022-09-21 00:35:04 -0700

    task 13first attempt

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *curr, *nxt, *prev;
+
+	curr = malloc(sizeof(listint_t));
+	if (curr == NULL && head == NULL)
+	{
+		free(curr);
+		return(NULL);
+	}
+	curr->n = number;
+/*	if (*head == NULL)
+	{
+		*head = curr;
+		*head->next = NULL;
+		free(curr);
+		return (*head);
+	}*/
+	prev = *head;
+	nxt = prev->next;
+	while(nxt != NULL)
+	{
+		if (nxt->n >= number) 	
+		{
+			prev->next = curr;
+			curr->next = nxt;
+			break;
+		}
+		else
+		{
+			prev = prev->next;
+			nxt = nxt->next;
+		}
+	}
+	return(curr);
+}
