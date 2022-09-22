e5484c9bc47abcd92874a79bcd7a795a97f3f76a
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-09-22 13:02:18 +0300

    add argv 1 condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
+    elif len(sys.argv) == 1:
+        print("0 arguments")
     elif len(sys.argv) > 2:    
         i = 1
         print("{} arguments:".format(len(sys.argv)))
