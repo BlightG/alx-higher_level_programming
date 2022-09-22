d738df31f5ebe5acfcee985341fd03abab8e3067
BlightG <birhane.gulilat@gmail.com>
2022-09-22 03:33:48 -0700

    function

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     elif len(sys.argv) == 1:
-        print("0 arguments")
+        print("0 arguments.")
     elif len(sys.argv) > 2:
         i = 1
         print("{} arguments:".format((len(sys.argv) - 1)))
