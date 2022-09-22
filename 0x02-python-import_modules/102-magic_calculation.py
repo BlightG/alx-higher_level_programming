8f0d27ff23382000980317a00d63997c03c69023
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-09-22 21:37:41 +0300

    bytecode second atemmpt

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add, sub = __import('magic_calculation_102').add, __import('magic_calculation_102').sub,
+    add, sub = __import__('magic_calculation_102').add,\
+               __import__('magic_calculation_102').sub,
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
