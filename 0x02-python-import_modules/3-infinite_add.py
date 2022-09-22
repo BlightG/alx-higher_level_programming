6e6ffcf23c423e22c40742ec9c67af34b606a43c
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-09-22 11:49:30 +0300

    task 3 first attempt

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+i = 1
+summ = 0
+if len(sys.argv) > 1:
+    while i < len(sys.argv):
+        summ = summ + int(sys.argv[i])
+        i += 1
+print("{}".format(summ))
