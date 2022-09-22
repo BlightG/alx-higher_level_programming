acd504a684e837d47053b6d43ce7c302988bf989
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-09-22 21:34:06 +0300

    bytocode 3

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    add, sub = __import('magic_calculation_102').add, __import('magic_calculation_102').sub,
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+
+    return sub(a, b)
