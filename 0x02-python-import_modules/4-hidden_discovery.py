5c3ee89e9ae42f20c4261000fd293ea5f9be0612
BlightG <birhane.gulilat@gmail.com>
2022-09-22 03:52:27 -0700

    fixed issue with if statments

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ = '__main__':
+if __name__ == '__main__':
     import hidden_4
     hiddir = dir(hidden_4)
     for i in range(0, len(hiddir)):
-        if hiddir[i][0] != "__":
+        if hiddir[i][0] != "_":
             print("{}".format(hiddir[i]))
