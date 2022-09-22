dc3df1b587d8cca65fcd826e81e3723ee5e842c8
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-09-22 19:50:48 +0300

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,9 +3,9 @@ if __name__ == '__main__':
     import sys
     import calculator_1 as calc
 
-    if len(sys.argv) == 3:
-        a = sys.argv[1]
-        b = sys.argv[3]
+    if len(sys.argv) == 4:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
         if sys.argv[2] == "+":
             print("{} + {} = {}".format(a, b, calc.add(a, b)))
         elif sys.argv[2] == "-":
