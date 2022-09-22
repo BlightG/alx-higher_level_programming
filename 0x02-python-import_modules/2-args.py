4947b75ec2922f805fc4dc9dd5d86719058b005f
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-09-22 13:17:40 +0300

    modified the position of if name == main

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import sys
+
+import sys
+
+
+def main():
     if len(sys.argv) == 2:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     elif len(sys.argv) == 1:
         print("0 arguments")
-    elif len(sys.argv) > 2:    
+    elif len(sys.argv) > 2:
         i = 1
         print("{} arguments:".format(len(sys.argv)))
         while i <= len(sys.argv):
             print("{}: {}".format(i, sys.argv[i]))
             i += i
+
+
+if __name__ == '__main__':
+    main()
