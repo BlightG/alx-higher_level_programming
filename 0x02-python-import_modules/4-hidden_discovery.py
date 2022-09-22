88a2cd2d01f09755911a0be6ca10310d2830a555
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-09-22 13:44:37 +0300

    first attempt

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ = '__main__':
+    import hidden_4
+    hiddir = dir(hidden_4)
+    for i in range(0, len(hiddir)):
+        if hiddir[i][0] != "__":
+            print("{}".format(hiddir[i]))
