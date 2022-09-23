fdf1ccc7da5b4fc47fef12439b60a02fa698351a
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-09-23 14:31:57 +0300

    second push

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+def delete_at(my_list=[], idx=0):
+    new_list = my_list [:]
+    my_list.clear()
+    for i in range(len(new_list)):
+        if i != idx:
+            my_list.append(new_list[i])
+    return my_list
