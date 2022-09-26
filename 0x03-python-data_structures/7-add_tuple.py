571535490ef11b5c86efcc5bde047713fda89ccc
BlightG <birhane.gulilat@gmail.com>
2022-09-26 02:06:01 -0700

    modification for task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,8 +3,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     list_b = list(tuple_b)
     if len(tuple_a) < 2:
-        for i in range(len(list_b), 2):
-            list_b.append(0)
+        for i in range(len(list_a), 2):
+            list_a.append(0)
     if len(tuple_b) < 2:
         for i in range(len(list_b), 2):
             list_b.append(0)
