2256ba535cc566c524228c5ec7a565b4c9ed75a2
BlightG <birhane.gulilat@gmail.com>
2022-09-26 02:00:41 -0700

    added guard for null sentence condition to task 8

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}{}".format(matrix[i][j],\
+            print("{:d}{}".format(matrix[i][j],
                   " " if j != len(matrix[i]) - 1 else ''), end="")
         print()
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_a = (len(sentence), sentence[0])
+    tuple_a = (len(sentence), sentence[0] if len(sentence) != 0 else 'None')
     return tuple_a
