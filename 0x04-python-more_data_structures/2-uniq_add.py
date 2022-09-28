a8770b56e658ef7220c77e5608db58d506ce3080
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-09-28 19:51:36 +0300

    added guard for edge case

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     newlist = my_list[:]
-    newlist.sort()
-    summ = newlist[0]
-    for i in range(1, len(newlist)):
-        if newlist[i] != newlist[i - 1]:
-            summ = newlist[i] + summ
-        else:
-            continue
-    return summ
+    if bool(my_list):
+        newlist.sort()
+        summ = newlist[0]
+        for i in range(1, len(newlist)):
+            if newlist[i] != newlist[i - 1]:
+                summ = newlist[i] + summ
+            else:
+                continue
+        return summ
+    else:
+        return 0
