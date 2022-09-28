eb613edcaaf850e85be64d64ddae0857e39112cc
BlightG <birhane.gulilat@gmail.com>
2022-09-28 10:31:45 -0700

    pycodestyle and chmod

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    newlist = list(map(lambda x : x * number, my_list))
+    newlist = list(map(lambda x: x * number, my_list))
     return newlist
