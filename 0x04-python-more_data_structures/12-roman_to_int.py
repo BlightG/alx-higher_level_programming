66268eff8b9b5e2c50defd480285d57f3beca13a
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-09-28 21:39:39 +0300

    task 1 first attempt

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string:
+        legend =\
+            {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        maxx = intt = 0
+        for i in reversed(range(len(roman_string))):
+            if roman_string[i] in legend:
+                if legend[roman_string[i]] > maxx:
+                    maxx = legend[roman_string[i]]
+                if legend[roman_string[i]] < maxx:
+                    intt = intt - legend[roman_string[i]]
+                else:
+                    intt = legend[roman_string[i]] + intt
+        return intt
+    else:
+        return 0
