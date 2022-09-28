68dd6bfd61ecba92583fe94d0275bcfd292065ab
BlightG <birhane.gulilat@gmail.com>
2022-09-28 10:39:37 -0700

    changed line lenght

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    newlist = list(map(lambda x: x * number, my_list))
-    return newlist
+    return list(map(lambda x: x * number, my_list))
