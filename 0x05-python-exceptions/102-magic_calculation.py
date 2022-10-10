5f65a15058b5057963448f954aac045a7b08f63b
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-10 19:21:05 +0300

    magic caluclations

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result = ((b ** a) / i) + b
+        except:
+            result = a + b
+    return result
