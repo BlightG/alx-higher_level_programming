7e2f010a346605ef3fa904f51dcafa7dfcc4d372
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-10 15:44:59 +0300

    second draft of all mandatory challenges

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    div_list = []
+    for i in range(0, list_length):
+        try:
+            div_list.append(my_list_1[i] / my_list_2[i])
+
+        except ZeroDivisionError:
+            div_list.append(0)
+            print("division by 0")
+        except TypeError:
+            div_list.append(0)
+            print("wrong type")
+        except IndexError:
+            div_list.append(0)
+            print("out of range")
+
+    return div_list
