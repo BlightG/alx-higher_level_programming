48b00f4c91d6ef6172e7d1106bc43387f4dd19df
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-10 17:10:44 +0300

    added a finally statment for 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    i = 0
     div_list = []
-    for i in range(0, list_length):
+    while i < list_length:
         try:
             div_list.append(my_list_1[i] / my_list_2[i])
 
@@ -14,5 +15,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             div_list.append(0)
             print("out of range")
+        finally:
+            i += 1
 
     return div_list
