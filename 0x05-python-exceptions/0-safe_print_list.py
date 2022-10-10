e569a2026432df93d4ff5b75096b44081b768ddd
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-10 16:21:54 +0300

    added exception to task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
         print()
         return i + 1
-    except IndexError:
+    except (IndexError, TypeError):
         print()
         return i
