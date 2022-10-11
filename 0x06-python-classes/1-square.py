7f0a2f284f1a40513ca717bc899bf87165416fd6
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-11 15:19:33 +0300

    first draft of task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Define a Square """
+
+
+class Square:
+    """ Define a Square"""
+    def __init__(self, _size=0):
+        """ define a square size"""
+        self._Square_size = _size
