649747e73bee694e5b161107df58539cf584b13b
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-11 18:11:38 +0300

    first draft of task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Define a Square """
+
+
+class Square:
+    """ Define a Square"""
+    def __init__(self, size=0):
+        """ define a square size"""
+        try:
+            if size < 0:
+                raise ValueError
+            else:    
+                self.__size = size
+        except TypeError as te:
+                print('size must be an integer', end="")
+                raise Exception
+        except ValueError as ve:
+            print('size must be >= 0', end="")
+            raise Exception
