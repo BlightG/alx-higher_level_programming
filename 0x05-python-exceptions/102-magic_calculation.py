581141f53b9d352d613de294a861ffceb6849fbe
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-11 11:48:05 +0300

    revert modification

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (b ** a) / i
-        except:
+        except Exception:
             result = a + b
             break
     return result
