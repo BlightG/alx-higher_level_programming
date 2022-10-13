dbf74cc6d0a7055c6c5b05faeb039ca960187431
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-13 12:41:36 +0300

    addedguard against - and incomplete tuple argument

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -24,7 +24,7 @@ class Square:
 
         if not isinstance(a, int) or not isinstance(b, int):
             raise TypeError("position must be a tuple of 2 positive integers")
-            
+
         self.__position = position
 
     @property
