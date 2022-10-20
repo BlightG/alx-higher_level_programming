9308bce16d05ba762633de939cafd65a38fb9fa7
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-20 15:24:45 +0300

    added more test case

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -3,7 +3,7 @@ matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 try:
     matrix = [[3, 9], [12, 15, 3]]
-    print(matrix_divided(matrix, 2))
+    print(matrix_divided(matrix))
     print(matrix)
 except Exception as e:
     print(e)
