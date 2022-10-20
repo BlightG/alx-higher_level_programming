eb5dddb3d8b7ffb5d45ef3b0f86b04f4d06e9d36
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-20 16:35:03 +0300

    added flaot(inf)

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 try:
-    matrix = ["[3, 9, 2]", [12, 15, 3]]
+    matrix = [[3, 9, 2], [12, 15, 3]]
     print(matrix_divided(matrix, 2))
     print(matrix)
 except Exception as e:
