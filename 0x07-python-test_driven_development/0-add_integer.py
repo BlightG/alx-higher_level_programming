550077ad540b3e1ac14b42002f97abe92074b6c9
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-20 14:31:06 +0300

    added more test cases for case 0-addint

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,6 +10,11 @@ def add_integer(a, b=98):
     """ add_integer (a, b)
         returns the sum of a and b
     """
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
+
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
         exit()
@@ -18,10 +23,7 @@ def add_integer(a, b=98):
         raise TypeError('b must be an integer')
         exit()
 
-    if isinstance(a, float):
-        a = int(a)
-
-    if isinstance(b, float):
-        b = int(b)
-
-    return a + b
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
+    return int(a) + int(b)
