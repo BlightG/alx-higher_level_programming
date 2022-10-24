8a7d0c45ffc725754e576e34e23538f6b592796c
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-24 13:21:46 +0300

    asdf

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,6 +2,7 @@
 """ file contaning a function lookup"""
 
 def lookup(obj):
+    """ a function that looks up all the attributes of an object"""
     objdict = [i for i in obj.__dict__]
     return objdict
     
