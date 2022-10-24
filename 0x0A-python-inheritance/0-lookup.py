edf77b83b5197e57e9d680019217b05bd857e990
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-24 21:28:57 +0300

    adslkfj

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,5 +4,5 @@
 
 def lookup(obj):
     """ a function that looks up all the attributes of an object"""
-    objdict = [[method_name for method_name in dir(obj)]]
+    objdict = [method_name for method_name in dir(obj)]
     return objdict
