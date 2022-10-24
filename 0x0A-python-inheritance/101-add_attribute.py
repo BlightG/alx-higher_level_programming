03eb1380ba7c1ee80460b76d8ac879fdb95e2eeb
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-25 00:18:08 +0300

    pycodestyle

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,21 +4,15 @@
 
 def add_attribute(obj, attr, val):
     """ a function that adds or modifies an attribute to an object
-    
+
         Args:
             obj: object to be moodified
             attr: a string value of the attributw to be moddfied or added
             val: new value of the modifed attribute
-        
+
         Return:
             returns an object with etra attribute
     """
     if not hasattr(obj, attr) and not hasattr(obj, '__dict__'):
         raise TypeError('can\'t add new attribute')
     setattr(obj, attr, val)
-
-
-
-
-
-
