eb89343e734dba5a5e9e7d9f8cc6488e5b55151a
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-24 15:41:45 +0300

    6 b4 1

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" started for base geometry"""
+
+
+class BaseGeometry:
+    """ Empty class base geomety"""
+    def area(self):
+        """ a module not yet implemented """
+        raise Exception('area() is not implemented')
