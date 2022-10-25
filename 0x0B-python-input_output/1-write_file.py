bd9942ff4158791ee6af296e33199edcb829f52a
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-25 13:13:57 +0300

    i alsodont kno

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,5 @@
 def write_file(filename="", text=""):
     ''' file length '''
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(text)
-        fcount = f.tell()
+        fcount = f.write(text)
         return fcount
