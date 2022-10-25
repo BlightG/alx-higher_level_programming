1b3543d9aee01e9ab79c3d2e12987fb3800dc920
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-25 17:39:54 +0300

    2nd |-|)

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,20 +5,19 @@ import sys
 import os
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-#read_file = __import__("0-read_file").read_file
+
 
 def write_to_json():
     """ a function to write a to json """
     if not os.path.exists('add_item.json'):
-        my_list = []
-        print("not exist")
+        mylist = []
         with open('add_item.json', 'w+', encoding='utf-8') as f:
-            save_to_json_file(my_list, 'add_item.json')
-    
+            save_to_json_file(mylist, 'add_item.json')
+
     mylist = load_from_json_file('add_item.json')
     for i in range(1, len(sys.argv)):
         mylist.append(sys.argv[i])
-        print(mylist)
     save_to_json_file(mylist, 'add_item.json')
+
 
 write_to_json()
