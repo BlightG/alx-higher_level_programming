e1e85634286a080a35fad465af23a135e08541ce
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-25 17:23:19 +0300

    7 1st |-|)

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,6 @@ import json
 def load_from_json_file(filename):
     """ print my_obj to file @filename """
     with open(filename, 'r', encoding='utf-8') as f:
-        return json.loads(f.read())
+        if f.read() != 0:
+            f.seek(0, 0)
+            return json.loads(f.read())
