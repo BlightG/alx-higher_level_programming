72f76e797fbd1fa63881bf61967e5d373825a414
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-25 19:49:07 +0300

    T0.6.1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,8 +7,8 @@ def read_file(filename=""):
         A module for reading a UTF-8  encoded file and prints to stdout
 
         Args:
-            filename: the name of the file 
+            filename: the name of the file
             with or without file path as a string
     """
-    with open(filename, 'r', encoding="utf-8") as readfile:
-        readfile.read()
+    with open(filename, 'r', encoding="utf-8") as f:
+        f.read()
