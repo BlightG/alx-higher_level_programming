0f2a4c9d8ce6fa046c12dc116ae377646dd31292
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-31 23:53:26 +0300

    travel

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" test module for Square """
+import unittest
+from models.square import Square
+
+
+
+class TestRectangle(unittest.TestCase):
+    """ A set of tests for Rectangle Class """
+    def test_rectangle_Equal(self):
+        """ Checks sucesses case of Base class """
+        r1 = Square(10, 2)
+        self.assertEqual(r1.id, 1)
+        r2 = Square(10, 2, 0, 0, 12)
+        dict = {'x': 0, 'y': 0, 'id': 1, 'height': 2, 'width': 10}
+        self.assertEqual(r1.to_dictionary(), dict)
+
+    def test_square_Exception(self):
+        """ Checks the Exceptions for Rectangle """
+        self.assertRaises(ValueError, Square, 10, -2)
+        self.assertRaises(TypeError, Square)
+        self.assertRaises(ValueError, Square, 10, 2, -4, 0)
