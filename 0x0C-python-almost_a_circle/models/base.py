5e2af1573413e9d5756d1051ad13df2cf75baad2
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-31 22:01:19 +0300

    moveee

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,7 +34,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """ converts a string representation to list """
-        if json_string is None or len(json_string) == 0:
+        if json_string is not None or len(json_string) != 0:
             return json.loads(json_string)
         return []
 
