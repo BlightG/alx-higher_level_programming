7763b83d3fe0999f866581181def1a41f35f7275
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-11-30 20:58:54 +0300

    more_push

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,3 @@
 --  a script that lists all the cities of California that can be found in the database
-select cities.id, cities.name
-from cities
+select cities.id, cities.name from cities
 where cities.state_id = '1';
