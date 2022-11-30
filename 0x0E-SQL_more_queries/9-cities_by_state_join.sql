c2ed3fdadee25f92bdd0afadfbb62df1e0b98050
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-11-30 21:23:16 +0300

    changed right to left jooin

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name FROM cities
-RIGHT JOIN states
+LEFT JOIN states
 ON cities.state_id = states.id;
