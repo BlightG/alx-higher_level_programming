9de73c7800e0308c5be3e7f7b22e2813b21136d7
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-11-30 21:05:04 +0300

    capitalization

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,4 @@
 --  a script that lists all the cities of California that can be found in the database
-select cities.id, cities.name from cities
-where cities.state_id = '1';
+SELECT cities.id, cities.name FROM cities
+WHERE cities.state_id = '1'
+ORDER BY cities.id ASC;
