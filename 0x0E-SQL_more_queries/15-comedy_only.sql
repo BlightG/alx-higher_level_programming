87496864472fe8a1bc48013ce1c3be763895bfab
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-01 19:11:40 +0300

    added comment at first line

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,4 @@
+-- a script that lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT tv_shows.title FROM tv_shows
 RIGHT JOIN tv_show_genres
 ON tv_show_genres.genre_id = '5' AND tv_show_genres.show_id = tv_shows.id
