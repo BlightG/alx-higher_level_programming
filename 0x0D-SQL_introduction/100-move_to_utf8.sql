477907e71e96c641f525fc4ff50c795ace4d24be
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-01 22:06:37 +0300

    add

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,2 @@
 --  convert all of the following to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci;
