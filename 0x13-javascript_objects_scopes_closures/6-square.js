847e183ad726053a86c562e361b0c9b7b39e4e46
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-20 17:22:33 +0300

    semisstandard linter fixing

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
 
 const Square5 = require('./5-square');
-
 class Square extends Square5 {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
