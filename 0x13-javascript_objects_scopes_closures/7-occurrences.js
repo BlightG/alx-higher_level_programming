bb23c011c69143355664abb627dbe27791a19b35
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-20 21:25:57 +0300

    fixed semistandard and corrected camel case

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-        let element_count = 0;
-        for (let i = 0 ; i < list.length ; i++){
-                if (list[i] === searchElement) {
-                        element_count += 1;
-                } 
-        }
-        return element_count;
-}
+  let elementcount = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      elementcount += 1;
+    }
+  }
+  return elementcount;
+};
