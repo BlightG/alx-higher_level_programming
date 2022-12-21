cb1f31af1afefb689c1ed6b670b5686c7786e829
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-21 08:38:28 +0300

    task 6 modefiied python is coo => cool

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let list = ['C is fun', 'Python is coo', 'JavaScript is amazing'];
+let list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++) {
   console.log(list[i]);
