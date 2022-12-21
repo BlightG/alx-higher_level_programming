ebe175d68051035b9fdec429c686bcf16e22a3d2
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-21 08:40:40 +0300

    task 7 c is fun => C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const { argv } = require('process');
 let num = parseInt(argv[2], 10);
 if (!isNaN(num)) {
   for (let i = 0; i < num; i++) {
-    console.log('c is fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
