164e22c5df09f894a3dd38626b2bbf6a100d181b
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-21 08:46:21 +0300

    task 8 changed # to X

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-let num = parseInt(argv[2], 10);
+const num = parseInt(argv[2], 10);
 if (!isNaN(num)) {
   for (let i = 0; i < num; i++) {
     for (let j = 0; j < num; j++) {
-      process.stdout.write('#');
+      process.stdout.write('X');
     }
     console.log('');
   }
