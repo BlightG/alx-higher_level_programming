a8bea5efb38f57a3ffb466140130af57d153b104
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-21 10:34:37 +0300

    task 100 semistandard

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const data = require('./100-data').list;
 
-const newList = data.map((element, index) => element * index );
+const newList = data.map((element, index) => element * index);
 console.log(data);
 console.log(newList);
