8ec51037c0061a288defb6c8a375a9e908ee7db1
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-21 08:30:48 +0300

    task 3 only print first argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,5 @@ const { argv } = require('process');
 if (argv[2] == null) {
   console.log('No argument');
 } else {
-  argv
-    .filter((element, index) => index > 1)
-    .forEach((element) => console.log(element));
+  console.log(argv[2]);
 }
