fa93d3f8230ca0d4488f8ef15629be6d955ca9c1
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-22 21:46:53 +0300

    print modfication

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
     for row in rows:
-        print("{},".format(row))
+        print("{}".format(row))
