068b7382c0bc60df23f6e365f2a40c15b0df9254
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-26 21:02:58 +0300

    task 8 print first elemnt

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""prints the first state object from database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy.sql import select
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__" and len(sys.argv) == 4:
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    con = engine.connect()
+    s = select([State]).where(State.id == '1')
+    result = con.execute(s)
+    for row in result:
+        print(f"{row[0]}: {row[1]}")
