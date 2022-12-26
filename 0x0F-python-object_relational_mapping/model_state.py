16d8307b7b3c744f608bc444dd5497cdf5a84f04
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-26 18:11:50 +0300

    task 6 first push

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Sql alchemy first trial """
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# manage tables
+Base = declarative_base()
+
+
+class State(Base):
+    """ states class to maniulate states table in hbtn_0e_6_usa """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                autoincrement=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
