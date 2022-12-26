f88792fda400f797deb5167bacf84e61fbb45437
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-26 19:51:42 +0300

    task 7

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Sql alchemy first trial """
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 # manage tables
@@ -10,6 +10,6 @@ Base = declarative_base()
 class State(Base):
     """ states class to maniulate states table in hbtn_0e_6_usa """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True,
+    id = Column(Integer, primary_key=True, 
                 autoincrement=True, nullable=False, unique=True)
-    name = Column(String(128), nullable=False)
+    name =  Column(String(128), nullable=False)
