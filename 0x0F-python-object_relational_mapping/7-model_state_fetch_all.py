66b2ea6a69b86cfb9f79914b2a13d567550fcda6
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-26 21:58:38 +0300

    task 8 minor change

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,7 @@ if __name__ == "__main__" and len(sys.argv) == 4:
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    # Base.metadata.create_all(engine)
     con = engine.connect()
     s = select([State])
     result = con.execute(s)
