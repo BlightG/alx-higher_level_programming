50a026dae35010eef5f526e3d3e839192ff2c555
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-26 23:03:11 +0300

    task 9 name like %a%

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from sqlalchemy.sql import select
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__" and len(sys.argv) == 4:
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    con = engine.connect()
+    s = select([State]).where(State.name.like('%a%')).order_by(State.id)
+    result = con.execute(s)
+    for row in result:
+        print(f"{row[0]}: {row[1]}")
