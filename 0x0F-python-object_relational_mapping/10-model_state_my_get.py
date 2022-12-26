1c5a8d6798f2cb776e54190ca9ba847766b06b68
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-26 23:12:17 +0300

    task 10 search for argv[4] in table

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from sqlalchemy.sql import select
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__" and len(sys.argv) == 5:
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    con = engine.connect()
+    s = select([State]).where(State.name == (sys.argv[4])).order_by(State.id)
+    result = con.execute(s)
+    if result:
+        for row in result:
+            print(f"{row[0]}")
+    else:
+        print("Not found")
