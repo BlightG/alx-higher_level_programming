43cf0b3c0d8664f588a365ad6711cdb0113419bc
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-26 22:11:29 +0300

    task 8 using session instead of query

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,6 +4,7 @@
 import sys
 from sqlalchemy.sql import select
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 
@@ -11,11 +12,9 @@ if __name__ == "__main__" and len(sys.argv) == 4:
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    con = engine.connect()
-    s = select([State]).where(State.id == '1')
-    result = con.execute(s)
-    row = result.fetchmany(1)
-    #print(row)
-    #for row in result:
-    print(f"{row[0][0]}: {row[0][1]}")
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+    print("Nothing" if not state else state)
