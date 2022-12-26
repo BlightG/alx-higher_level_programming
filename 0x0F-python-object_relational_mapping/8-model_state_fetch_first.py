87d5a506c5fe50df2f5e43d9a2951043650678b9
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-26 22:49:31 +0300

    added an else statment

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,7 +12,7 @@ if __name__ == "__main__" and len(sys.argv) == 4:
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
