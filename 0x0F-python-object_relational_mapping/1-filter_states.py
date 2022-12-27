42887dde483fc088b21ca30df1c8bab13d149e78
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-27 16:54:10 +0300

    q and 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-""" first sqldb trial """
-import MySQLdb
-from sys import argv
+'''Python script to list all states with a
+name starting with N from the db hbtn_0e_0_usa'''
 
-if __name__ == "__main__" and len(argv) == 4:
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+import sys
+import MySQLdb
+
+if len(sys.argv) > 3:
+    user_name = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost",
+                         user=user_name,
+                         passwd=passwd,
+                         db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' OR 'n%'\
-                ORDER BY states.id ASC;")
-    rows = cur.fetchall()
-    for row in rows:
-        print("{}".format(row))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%';")
+    states = cur.fetchall()
+    for row in states:
+        print(row)
     cur.close()
     db.close()
+else:
+    print(f"Usage: ./1-filter_states.py \
+    <mysql_username> <mysql_password> <database_name>")
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
-""" first sqldb trial """
-import MySQLdb
-from sys import argv
+'''script that takes an argument and displays
+all values in the states table of hbtn_0e_0_usa where
+name matches the argument'''
 
-if __name__ == "__main__" and len(argv) == 5:
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+import sys
+import MySQLdb
+
+if len(sys.argv) > 4:
+    user_name = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    db = MySQLdb.connect(host="localhost",
+                         user=user_name,
+                         passwd=passwd,
+                         db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' \
-                ORDER BY states.id ASC;".format(argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print("{}".format(row))
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{0}';".format(
+        state_name)
+    cur.execute(query)
+    states = cur.fetchall()
+    for row in states:
+        print(row)
     cur.close()
     db.close()
+else:
+    print(f"Usage: ./2-my_filter_states.py \
+    <mysql_username> <mysql_password> <database_name> <state_name>")
