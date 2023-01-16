4df7986ea10ebb4b3d9db9ba7a765afa52eba62f
BlightG <birhane.gulilat@gmail.com>
2023-01-16 17:35:36 +0300

    removed last new line

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends GET request and displays body for 200 ok response
-http_response=$(curl -s -o response.txt -w "%{http_code}" "$1");if [ "$http_response" == "200" ]; then cat response.txt; rm response.txt;fi
+http_response=$(curl -s -o response.txt -w "%{http_code}" "$1");if [ "$http_response" == "200" ]; then echo -n "$(cat response.txt)"; rm response.txt;fi
