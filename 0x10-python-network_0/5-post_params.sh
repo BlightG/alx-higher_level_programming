5eb13b6450f20b664b9ca19974b8db85ee5c7546
BlightG <birhane.gulilat@gmail.com>
2023-01-16 19:55:01 +0300

    useed -d instead of -H

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post to a passed url
-curl -s -X POST "$1" -H "email:	test@gmail.com" -H "subject: I will always be here for PLD"
+curl -s -X POST  -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
