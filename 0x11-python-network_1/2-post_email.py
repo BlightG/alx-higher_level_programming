d8542d6701415bda709dbeaee859701a4e966ea2
BlightG <birhane.gulilat@gmail.com>
2023-01-17 18:12:09 +0300

    put gurad so code wont run when imported

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,10 +5,11 @@ import urllib.request
 # import urllib.parse
 
 
-values = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(sys.argv[1], data)
-with urllib.request.urlopen(req) as responses:
-    response = responeses.read()
-    print(f'your email is: {response.decode("utf8")}')
+if __name__ == '__main__':
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as responses:
+        response = responeses.read()
+        print(f'your email is: {response.decode("utf8")}')
