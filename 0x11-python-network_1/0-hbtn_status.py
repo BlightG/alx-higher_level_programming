b7d168edeed1674096bfdfe74a64c619269a338c
BlightG <birhane.gulilat@gmail.com>
2023-01-17 15:15:16 +0300

    task the first modified putput spacing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as req:
     response = req.read()
     print('Body response:')
-    print(f'\t- type: {type(response)}')
-    print(f'\t- content: {response}')
-    print(f'\t- utf8 content:{response.decode("utf8")}')
+    print(f'   - type: {type(response)}')
+    print(f'   - content: {response}')
+    print(f'   - utf8 content:{response.decode("utf8")}')
