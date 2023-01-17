05645d62f422654f14db9dcdaeafbe4b76eaffd2
BlightG <birhane.gulilat@gmail.com>
2023-01-17 22:20:58 +0300

    simplified version

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,5 +6,4 @@ import sys
 
 if __name__ == '__main__':
     req = requests.post(sys.argv[1], data={'email', sys.argv[2]})
-    response = requests.get(sys.argv[1])
-    print(f'Your email is: {response.txt}')
+    print(req.text)
