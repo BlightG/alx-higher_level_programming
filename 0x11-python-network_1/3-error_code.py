d1ade99bd0575e75732922f7c5272df0e40ea309
BlightG <birhane.gulilat@gmail.com>
2023-01-17 18:26:39 +0300

    task 3 first push

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" send s requespt and prints response body while managing error """
+import urllib.request
+import sys
+import urllib.error
+
+
+if __name__ == '__main__':
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as responses:
+            print(responses.read())
+    except urllib.error.HTTPError as e:
+        print(f'Error code: {e.code}')
+        
