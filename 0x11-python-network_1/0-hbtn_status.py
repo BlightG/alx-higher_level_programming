2cdbe41fe7755a742a8e6401d392802fa7a2f72e
BlightG <birhane.gulilat@gmail.com>
2023-01-17 15:11:14 +0300

    task the first

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""script that fetches a static URl"""
+import urllib.request
+
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as req:
+    response = req.read()
+    print('Body response:')
+    print(f'\t- type: {type(response)}')
+    print(f'\t- content: {response}')
+    print(f'\t- utf8 content:{response.decode("utf8")}')
