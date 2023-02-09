0f43337f9c24af34835993b05a9bf4b73106c38c
BlightG <birhane.gulilat@gmail.com>
2023-02-09 15:19:19 +0300

    changed consolge log prefix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,6 @@ const { argv } = require('process');
 request
   .get(argv[2])
   .on('response', function (response) {
-    console.log('Code: ', response.statusCode); // 200
+    console.log('code:', response.statusCode); // 200
     // console.log(response.headers['content-type']) 'image/png'
   });
