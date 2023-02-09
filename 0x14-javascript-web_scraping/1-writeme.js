e11c5823ae3b7a5dff6afab3b5e6e1f1cf33a145
BlightG <birhane.gulilat@gmail.com>
2023-02-09 14:38:04 +0300

    semistandard fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const { argv } = require('process')
+const { argv } = require('process');
 
-let fInput = argv[3]
+const fInput = argv[3];
 fs.writeFile(argv[2], fInput, (err) => {
-   if (err) throw err;
-})
+  if (err) throw err;
+});
