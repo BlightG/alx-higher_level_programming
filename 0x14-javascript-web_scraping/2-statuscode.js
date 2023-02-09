4c438c0e1a3965a8d50756f7e5776fb7edde9cdd
BlightG <birhane.gulilat@gmail.com>
2023-02-09 15:12:58 +0300

    first push for task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const { argv } = require('process');
+
+request
+  .get(argv[2])
+  .on('response', function(response) {
+    console.log('Code: ', response.statusCode) // 200
+    // console.log(response.headers['content-type']) 'image/png'
+  })
