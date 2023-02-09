552ec0a93f2d24bf498eaf7d32f43239a72efbc0
BlightG <birhane.gulilat@gmail.com>
2023-02-09 20:50:06 +0300

    made files executable

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const { argv } = require('process');
+
+let count = 0;
+
+request(argv[2], (error, response, body) => {
+  if (error) console.log(error);
+  // console.log(response.statusCode);
+  const jsondict = JSON.parse(body);
+  for (let i = 0; i < jsondict['results'].length; i++) {
+    // console.log(jsondict['results'][i]);
+    for (let j = 0; j < jsondict['results'][i]['characters'].length; j++) {
+       // console.log(typeof jsondict['results'][i]['characters'][j]);
+       if (jsondict['results'][i]['characters'][j] ==
+             'https://swapi-api.alx-tools.com/api/people/18/') {
+         count += 1;
+    }
+  }
+}
+console.log(count)
+});
