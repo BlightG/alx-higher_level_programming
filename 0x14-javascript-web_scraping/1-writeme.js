39c1e0ff31cd25b65960a3416eb8880816945150
BlightG <birhane.gulilat@gmail.com>
2023-02-09 14:36:48 +0300

    removed new line

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const { argv } = require('process')
 
-let fInput = argv[3] + '\n'
+let fInput = argv[3]
 fs.writeFile(argv[2], fInput, (err) => {
    if (err) throw err;
 })
